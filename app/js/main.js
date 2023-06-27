2d27d6e78e068a0a07ffcbce4b469c07c05053e4
Serhii Dotsenko <serdotsenko@gmail.com>
2023-06-27 17:05:17 +0300

    13.7.2 customer feedback section

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,17 @@
-$(function () {
+$(() => {
   $('.slider').slick({
-    arrows: false,
-    dots: true
+    arrows: true,
+    dots: true,
+    prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev">' +
+      '<svg class="slider__arrow-img">' +
+      '<use xlink:href="images/sprite.svg#arrow-left"></use>' +
+      '</svg>' +
+      '</button>',
+    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next">' +
+      '<svg class="slider__arrow-img">' +
+      '<use xlink:href="images/sprite.svg#arrow-left"></use>' +
+      '</svg>' +
+      '</button>'
   });
 });
 
