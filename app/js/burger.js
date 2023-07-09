b2e880675f5eb46fc2cb2b89911142a1c70f9967
Serhii Dotsenko <serdotsenko@gmail.com>
2023-07-09 15:33:50 +0300

    14.12.0 pixel-perfect for adaptive design

--- app/js/burger.js
+++ app/js/burger.js
@@ -1,6 +1,19 @@
 export default function burger() {
   const burger = document.querySelector('.burger-button');
+  const bodyLock = document.querySelector('body');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
   burger.addEventListener('click', () => {
-    burger.classList.toggle('burger-button--active');
+    mobileMenu.classList.toggle('active');
+    if (mobileMenu.classList.contains('active')) {
+      bodyLock.classList.add('lock')
+    }
+  });
+
+  document.addEventListener('click', e => {
+    if (e.target !== burger && e.target !== mobileMenu) {
+      mobileMenu.classList.remove('active');
+      bodyLock.classList.remove('lock');
+    }
   });
 }
